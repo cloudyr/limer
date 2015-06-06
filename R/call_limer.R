cd657cd46368349014f197b3403417b35c96a20c
Andrew Heiss <andrew@andrewheiss.com>
2015-06-05 20:16:57 -0400

    Finish generic API function

--- R/call_limer.R
+++ R/call_limer.R
@@ -1,8 +1,39 @@
 #' Make a call to the LimeSurvey API
 #'
-#' This function makes a generic call to the LimeSurvey API,
+#' This function makes a generic call to the LimeSurvey API. See \url{https://manual.limesurvey.org/RemoteControl_2_API} for API documentation.
+#' @param method API function to call. Full lis Defaults to value set in \code{options()}.
+#' @param params Optional named list of parameters to pass to the function.
+#' @return Results from the API (sometimes plain text, sometimes base64-encoded text).
+#' @import httr
+#' @export
+#' @examples \dontrun{
+#' call_limer(method = "list_surveys")
+#' call_limer(method = "get_summary",
+#'            params = list(iSurveyID = 238481,
+#'                          sStatname = "completed_responses"))
+#' }
 #' @export
 
-call_limer <- function() {
-  return(NULL)
+call_limer <- function(method, params = list(), ...) {
+  if (!is.list(params)) {
+    stop("params must be a list.")
+  }
+
+  if (!exists("session_key", envir = session_cache)) {
+    stop("You need to get a session key first. Run get_session_key().")
+  }
+
+  key.list <- list(sSessionKey = session_cache$session_key)
+  params.full <- c(key.list, params)
+
+  body.json <- list(method = method,
+                    # This seems to not matter, but the API call breaks without it,
+                    # so just pass nothing. ¯\_(ツ)_/¯
+                    id = " ",
+                    params = params.full)
+
+  r <- POST(getOption('lime_api'), content_type_json(),
+            body = jsonlite::toJSON(body.json, auto_unbox = TRUE), ...)
+
+  return(unlist(jsonlite::fromJSON(content(r))$result))
 }
