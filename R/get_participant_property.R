704336667d6028517ce590c435ce72040a4d5f14
reperiam <ulrich.lang@swr.de>
2022-11-25 14:11:54 +0100

    linting code

--- R/get_participant_property.R
+++ R/get_participant_property.R
@@ -1,12 +1,14 @@
 #' Get a participant property from a LimeSurvey survey
 #'
-#' This function exports and downloads a participant property from a LimeSurvey survey.
+#' This function exports and downloads a participant property from a LimeSurvey
+#' survey.
 #' @param iSurveyID \dots
 #' @param aTokenQueryProperties \dots
 #' @param aTokenProperties \dots
 #' @export
 #' @examples \dontrun{
-#' get_participant_property(iSurveyID = 12345, aTokenQueryProperties = 1, aTokenProperties = list("attribute_1"))
+#' get_participant_property(iSurveyID = 12345, aTokenQueryProperties = 1, +
+#' aTokenProperties = list("attribute_1"))
 #' }
 
 get_participant_property <- function(iSurveyID, aTokenQueryProperties, aTokenProperties) {
